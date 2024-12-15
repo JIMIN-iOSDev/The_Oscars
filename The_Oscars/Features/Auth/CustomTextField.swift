f9455884372073d6a357d52af334979f5260e0c6
Eden <you8inpark@gmail.com>
2024-12-16 02:06:27 +0900

    feat: CustomTextField 레이아웃 지정

--- The_Oscars/Features/Auth/CustomTextField.swift
+++ The_Oscars/Features/Auth/CustomTextField.swift
@@ -14,11 +14,12 @@ class CustomTextField: UIView {
     private let textField = UITextField()
     private let errorLabel = UILabel()
     private let bottomBorder = UIView()
+    private let button = UIButton()
     
     // MARK: - 초기화
     init(title: String, placeholder: String) {
         super.init(frame: .zero)
-        setupUI()
+        setupUI(title: title, placeholder: placeholder)
     }
     
     required init?(coder: NSCoder) {
@@ -26,7 +27,45 @@ class CustomTextField: UIView {
     }
     
     // MARK: - UI 설정
-    private func setupUI() {
+    private func setupUI(title: String, placeholder: String) {
+        titleLabel.text = "제목"
+        titleLabel.font = UIFont.systemFont(ofSize: 12)
+        titleLabel.textColor = .lightGray
         
+        textField.placeholder = "제목을 입력해주세요"
+        textField.borderStyle = .none
+        textField.font = UIFont.systemFont(ofSize: 12)
+        
+        bottomBorder.backgroundColor = .gray
+        
+        errorLabel.font = UIFont.systemFont(ofSize: 8)
+        errorLabel.textColor = .red
+        errorLabel.isHidden = true
+        
+        addSubview(titleLabel)
+        addSubview(textField)
+        addSubview(bottomBorder)
+        addSubview(errorLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+        }
+        
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(40)
+        }
+        
+        bottomBorder.snp.makeConstraints { make in
+            make.top.equalTo(textField.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(1)
+        }
+        
+        errorLabel.snp.makeConstraints { make in
+            make.top.equalTo(bottomBorder.snp.bottom).offset(4)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
     }
 }
--- The_Oscars/Features/Auth/SignupViewController.swift
+++ The_Oscars/Features/Auth/SignupViewController.swift
@@ -10,5 +10,29 @@ import UIKit
 import SnapKit
 
 class SignupViewController: UIViewController {
+    private let idField = CustomTextField(title: "아이디", placeholder: "아이디를 입력해 주세요")
+    private let passwordField = CustomTextField(title: "비밀번호", placeholder: "비밀번호를 입력해 주세요")
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        view.addSubview(idField)
+        view.addSubview(passwordField)
+        
+        idField.snp.makeConstraints{ make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(80)
+        }
+        
+        passwordField.snp.makeConstraints { make in
+            make.top.equalTo(idField.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(80)
+        }
+    }
 }
