ee542d85ef568a0606a2315ae595b0ee322165a3
Jimin Seo <tig05015@naver.com>
2024-12-16 18:47:27 +0900

    [feat]: TapBar & SearchBar
    네비게이션 지우고 탭바에 맞춰서 검색창 구현
    별도의 검색 버튼 없이 엔터 누르면 검색이 가능하게 구현

--- The_Oscars/Features/Search/SearchViewController.swift
+++ The_Oscars/Features/Search/SearchViewController.swift
@@ -5,11 +5,10 @@
 //  Created by eden on 12/13/24.
 //
 
-<<<<<<< HEAD
 import UIKit
 import SnapKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITextFieldDelegate {
     
     // 검색창
     private let searchTextfield: UITextField = {
@@ -17,15 +16,8 @@ class SearchViewController: UIViewController {
         textfield.backgroundColor = .lightGray
         textfield.textColor = .black
         textfield.borderStyle = .roundedRect
-        textfield.attributedPlaceholder = NSAttributedString(string: "영화를 검색하세요", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        textfield.attributedPlaceholder = NSAttributedString(string: "영화 제목을 검색하세요", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         return textfield
-    }()
-    
-    // 뒤로가기 버튼
-    private let backButton: UIBarButtonItem = {
-        let button = UIBarButtonItem()
-        button.title = ""
-        return button
     }()
     
     // 추천 글씨
@@ -38,83 +30,45 @@ class SearchViewController: UIViewController {
         return label
     }()
     
-    // 임시 예매버튼
-    private lazy var testButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Test", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
-        button.addTarget(self, action: #selector(navigateToSearchScreen), for: .touchUpInside)
-        return button
-    }()
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .white
         
-        setNavigationBarHeight()
-        configureNavigationBar()
-        configureUI()
-    }
-    
-    // 임시 네비게이션
-    @objc func navigateToSearchScreen() {
-        let detailVC = MovieDetailController()
-        navigationController?.pushViewController(detailVC, animated: true)
-    }
-    
-    // 네비게이션 바 높이 조정
-    private func setNavigationBarHeight() {
+        searchTextfield.delegate = self
+        searchTextfield.becomeFirstResponder()
         
-        if let navBar = navigationController?.navigationBar {
-            var frame = navBar.frame
-            frame.origin.y = 80
-            navBar.frame = frame
-        }
-    }
-    
-    // 네비게이션 바 버튼 설정
-    private func configureNavigationBar() {
-        navigationItem.titleView = searchTextfield
-        navigationItem.backBarButtonItem = backButton
-        navigationController?.navigationBar.tintColor = .black
+        configureUI()
     }
     
     private func configureUI() {
 
-        [searchTextfield, recommendLabel, testButton]
+        [searchTextfield, recommendLabel]
             .forEach { view.addSubview($0) }
         
         searchTextfield.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(100)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(UIScreen.main.bounds.width - 40)
             $0.height.equalTo(50)
-            $0.width.equalTo(UIScreen.main.bounds.width)
         }
         
         recommendLabel.snp.makeConstraints {
-            $0.top.equalTo(searchTextfield.snp.bottom).offset(10)
-            $0.leading.equalToSuperview().offset(10)
+            $0.top.equalTo(searchTextfield.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(20)
         }
-        
-        testButton.snp.makeConstraints {
-            $0.center.equalToSuperview()
-            $0.width.height.equalTo(30)
+    }
+    
+    // 엔터 키 누르면 키보드 사라지고 검색이 가능
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let searchQuery = textField.text, !searchQuery.isEmpty {
+            print("검색할 영화: \(searchQuery)")
+            textField.resignFirstResponder()
+            return true
         }
+        return false
     }
     
 }
 
-
-#Preview {
-    SearchViewController()
-=======
-import Foundation
-import UIKit
-
-class SearchViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-    }
->>>>>>> develop
-}
