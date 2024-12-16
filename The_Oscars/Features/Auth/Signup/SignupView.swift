357e52762fea6f0899ac1e43c2440e856d0fb68c
Eden <you8inpark@gmail.com>
2024-12-16 16:41:06 +0900

    feat: 로그인 네비게이션 스택뷰 생성 및 레이아웃 구현

--- The_Oscars/Features/Auth/Signup/SignupView.swift
+++ The_Oscars/Features/Auth/Signup/SignupView.swift
@@ -25,6 +25,29 @@ class SignupView: UIView {
     private let emailField = CustomTextField(title: "이메일 주소", placeholder: "이메일 주소를 입력해 주세요")
     private let nameField = CustomTextField(title: "이름", placeholder: "OSCARS에서 사용할 이름을 입력해 주세요")
     private let phoneField = CustomTextField(title: "전화번호", placeholder: "번호를 '-' 제외하고 입력해 주세요")
+    private let loginNavgationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이미 회원이시신요?"
+        label.textColor = .darkGray
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textAlignment = .center
+        return label
+    }()
+    private let loginNavgationButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("로그인 하러 가기", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        return button
+    }()
+    private let loginNavgationStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 4
+        stackView.alignment = .center
+        stackView.distribution = .equalCentering
+        return stackView
+    }()
     private let signupButton: UIButton = {
         let button = UIButton()
         button.setTitle("회원가입 하기", for: .normal)
@@ -54,6 +77,10 @@ class SignupView: UIView {
         addSubview(nameField)
         addSubview(phoneField)
         addSubview(signupButton)
+        addSubview(loginNavgationStackView)
+        
+        loginNavgationStackView.addArrangedSubview(loginNavgationLabel)
+        loginNavgationStackView.addArrangedSubview(loginNavgationButton)
         
         titleLabel.snp.makeConstraints { make in
             make.top.equalTo(safeAreaLayoutGuide).offset(8)
@@ -84,11 +111,16 @@ class SignupView: UIView {
         nameField.snp.makeConstraints{ make in
             make.top.equalTo(emailField.snp.bottom).offset(40)
             make.leading.trailing.equalToSuperview().inset(20)
-
+            
         }
         
         phoneField.snp.makeConstraints{ make in
             make.top.equalTo(nameField.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        loginNavgationStackView.snp.makeConstraints { make in
+            make.top.equalTo(phoneField.snp.bottom).offset(30)
             make.leading.trailing.equalToSuperview().inset(20)
         }
         
