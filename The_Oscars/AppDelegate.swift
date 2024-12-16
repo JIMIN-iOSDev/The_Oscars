2b7f7f47267443313277fc7438275d4ed9fd70d2
“hyunwook-sevo” <seojy1276@naver.com>
2024-12-16 16:54:46 +0900

    feat: 로그인 페이지 구현

--- The_Oscars/AppDelegate.swift
+++ The_Oscars/AppDelegate.swift
@@ -13,12 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UINavigationController(rootViewController: ViewController())
-        window.makeKeyAndVisible()
-        self.window = window
-        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = LoginViewController() // 초기 ViewController 설정
+        window?.makeKeyAndVisible()
         return true
     }
 }
+
 
--- The_Oscars/Features/Auth/LoginViewController.swift
+++ The_Oscars/Features/Auth/LoginViewController.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    private let loginView = LoginView()
+    
+    override func loadView() {
+        view = loginView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupActions()
+    }
+    
+    
+    private func setupActions() {
+        loginView.loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        loginView.signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+        loginView.findIdPasswordButton.addTarget(self, action: #selector(findIdPasswordButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func loginButtonTapped() {
+        print("로그인 버튼이 눌렸습니다.")
+    }
+    
+    @objc private func signUpButtonTapped() {
+        print("회원가입 버튼이 눌렸습니다.")
+    }
+    
+    @objc private func findIdPasswordButtonTapped() {
+        print("아이디/비밀번호 찾기 버튼이 눌렸습니다.")
+    }
+}
