13376dd90c023d7ad3acb136bf6b99a375ba44fe
Jimin Seo <tig05015@naver.com>
2024-12-16 22:43:07 +0900

    [feat]: MovieDetailController

--- The_Oscars/Features/Movies/MovieDetail/MovieDetailController.swift
+++ The_Oscars/Features/Movies/MovieDetail/MovieDetailController.swift
@@ -17,25 +17,119 @@ class MovieDetailController: UIViewController {
         return image
     }()
     
+    // 영화 제목
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "삭제예정"
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    // 개봉일
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "삭제예정2"
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 15)
+        return label
+    }()
+    
+    // 누적 관객 수
+    private let audienceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "삭제예정3"
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 15)
+        return label
+    }()
+    
+    // 평점
+    private let gradeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "삭제예정4"
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 20)
+        return label
+    }()
+    
+    // 세부설명
+    private let explanationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "삭제예정5"
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 15)
+        return label
+    }()
+    
+    // 예매 버튼
+    private let bookingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("예매하기", for: .normal)
+        button.setTitleColor(.systemYellow, for: .normal)
+        button.layer.borderColor = UIColor.systemYellow.cgColor
+        button.layer.borderWidth = 2
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .white
         navigationItem.setHidesBackButton(true, animated: false)
-        navigationItem.leftBarButtonItem = nil
+
+        // 버튼 없이 제스쳐로 뒤로 가기 - 해결안됨
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+        
         configureUI()
         
     }
     
     private func configureUI() {
         
-        view.addSubview(movieImage)
+        [movieImage, titleLabel, dateLabel, audienceLabel, gradeLabel, explanationLabel, bookingButton]
+            .forEach { view.addSubview($0) }
         
         movieImage.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalToSuperview().offset(100)
-            $0.width.equalTo(view.bounds.width / 2)
-            $0.height.equalTo(view.bounds.height / 3)
+            $0.width.equalTo(view.bounds.width - 140)
+            $0.height.equalTo(view.bounds.height / 2)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(movieImage.snp.bottom).offset(25)
+            $0.leading.equalTo(movieImage.snp.leading)
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(15)
+            $0.leading.equalTo(movieImage.snp.leading)
+        }
+        
+        audienceLabel.snp.makeConstraints {
+            $0.top.equalTo(dateLabel.snp.bottom).offset(10)
+            $0.leading.equalTo(movieImage.snp.leading)
+        }
+        
+        gradeLabel.snp.makeConstraints {
+            $0.top.equalTo(dateLabel.snp.top).offset(7.5)
+            $0.trailing.equalTo(movieImage.snp.trailing)
+        }
+        
+        explanationLabel.snp.makeConstraints {
+            $0.top.equalTo(audienceLabel.snp.bottom).offset(30)
+            $0.leading.equalTo(movieImage.snp.leading)
+        }
+        
+        bookingButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.centerX.equalToSuperview()
+            $0.leading.equalTo(movieImage.snp.leading)
+            $0.trailing.equalTo(movieImage.snp.trailing)
+            $0.height.equalTo(40)
         }
     }
 }
--- The_Oscars/Features/Search/SearchViewController.swift
+++ The_Oscars/Features/Search/SearchViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
+class SearchViewController: UIViewController, UITextFieldDelegate {
     
     // 검색창
     private let searchTextfield: UITextField = {
@@ -30,28 +30,7 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionV
         return label
     }()
     
-    private var recommendedMovies: [(imageURL: String, title: String)] = [
-            ("https://image.tmdb.org/t/p/w500/movie1.jpg", "영화1"),
-            ("https://image.tmdb.org/t/p/w500/movie2.jpg", "영화2"),
-            ("https://image.tmdb.org/t/p/w500/movie3.jpg", "영화3"),
-            ("https://image.tmdb.org/t/p/w500/movie4.jpg", "영화4"),
-            ("https://image.tmdb.org/t/p/w500/movie5.jpg", "영화5"),
-            ("https://image.tmdb.org/t/p/w500/movie6.jpg", "영화6")
-        ]
-        
-        // CollectionView
-        private let collectionView: UICollectionView = {
-            let layout = UICollectionViewFlowLayout()
-            layout.scrollDirection = .vertical
-            layout.minimumLineSpacing = 10
-            layout.minimumInteritemSpacing = 10
-            layout.itemSize = CGSize(width: (UIScreen.main.bounds.width - 40) / 3, height: (UIScreen.main.bounds.width - 40) / 3 + 40) // 3개의 이미지
-            
-            let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-            collectionView.backgroundColor = .white
-            return collectionView
-        }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -60,18 +39,12 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionV
         searchTextfield.delegate = self
         searchTextfield.becomeFirstResponder()
         
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-        // Register custom cell for the collection view
-        collectionView.register(MovieCell.self, forCellWithReuseIdentifier: "movieCell")
-        
         configureUI()
     }
     
     private func configureUI() {
 
-        [searchTextfield, recommendLabel, collectionView]
+        [searchTextfield, recommendLabel]
             .forEach { view.addSubview($0) }
         
         searchTextfield.snp.makeConstraints {
@@ -85,11 +58,6 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionV
             $0.top.equalTo(searchTextfield.snp.bottom).offset(20)
             $0.leading.equalToSuperview().offset(20)
         }
-        
-        collectionView.snp.makeConstraints {
-                $0.top.equalTo(recommendLabel.snp.bottom).offset(20)
-                $0.left.right.bottom.equalToSuperview().offset(0)
-            }
     }
     
     // 엔터 키 누르면 키보드 사라지고 검색이 가능
@@ -101,80 +69,5 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionV
         }
         return false
     }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return recommendedMovies.count
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "movieCell", for: indexPath) as! MovieCell
-            
-            // Set image and title for each cell
-            let movie = recommendedMovies[indexPath.item]
-            cell.configure(imageURL: movie.imageURL, title: movie.title)
-            
-            return cell
-        }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-            let width = (UIScreen.main.bounds.width - 40) / 3 // 3 items per row
-            return CGSize(width: width, height: width + 40) // Set height based on image width
-        }
-    }
-
-    // MARK: - Custom UICollectionViewCell
-
-    class MovieCell: UICollectionViewCell {
-        
-        private let movieImageView: UIImageView = {
-            let imageView = UIImageView()
-            imageView.contentMode = .scaleAspectFill
-            imageView.clipsToBounds = true
-            return imageView
-        }()
-        
-        private let titleLabel: UILabel = {
-            let label = UILabel()
-            label.textColor = .black
-            label.textAlignment = .center
-            label.font = .systemFont(ofSize: 14)
-            return label
-        }()
-        
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            
-            contentView.addSubview(movieImageView)
-            contentView.addSubview(titleLabel)
-            
-            movieImageView.snp.makeConstraints {
-                $0.top.leading.trailing.equalToSuperview()
-                $0.height.equalTo(movieImageView.snp.width) // Make it square
-            }
-            
-            titleLabel.snp.makeConstraints {
-                $0.top.equalTo(movieImageView.snp.bottom).offset(5)
-                $0.leading.trailing.equalToSuperview()
-                $0.bottom.equalToSuperview()
-            }
-        }
-        
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        
-        func configure(imageURL: String, title: String) {
-            // Set the title
-            titleLabel.text = title
-            
-            // Load the image (You can use libraries like SDWebImage or Kingfisher to load images from URL)
-            if let url = URL(string: imageURL) {
-                // Here we'll use a simple way to load the image, but you can use a library like SDWebImage for caching.
-                let data = try? Data(contentsOf: url)
-                if let imageData = data {
-                    movieImageView.image = UIImage(data: imageData)
-                }
-            }
-        }
 }
 
