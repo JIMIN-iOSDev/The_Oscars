739a5e55d0543c0c0db9af627ec8672d9053d4f0
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2024-12-16 17:52:53 +0900

    fix: homeVC 를 movieListVC 로 변경함

--- The_Oscars/Features/Movies/TabBar/TabBarViewController.swift
+++ The_Oscars/Features/Movies/TabBar/TabBarViewController.swift
@@ -17,17 +17,17 @@ class TabBarViewController: UITabBarController {
     
     private func setupTabBar() {
         
-        let homeVC = HomeViewController()
+        let movieListVC = MovieListViewController()
         let searchVC = SearchViewController()
         let mypageVC = MyPageViewController()
         
         //탭바 아이템 설정
-        homeVC.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house.fill"), tag: 0)
+        movieListVC.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house.fill"), tag: 0)
         searchVC.tabBarItem = UITabBarItem(title: "검색", image: UIImage(systemName: "sparkle.magnifyingglass"), tag: 1)
         mypageVC.tabBarItem = UITabBarItem(title: "마이", image: UIImage(systemName: "rectangle.badge.person.crop"), tag: 2)
         
         self.tabBar.tintColor = .black
         
-        viewControllers = [homeVC, searchVC, mypageVC]
+        viewControllers = [movieListVC, searchVC, mypageVC]
     }
 }
