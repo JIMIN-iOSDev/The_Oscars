f5d69d0462b3727f6e0a4cfb2b308f5164c92b0b
solnamul <sol7113@gmail.com>
2024-12-17 20:23:07 +0900

    feat : 예매 인원 수 조절 기능 구현

--- The_Oscars/Features/Movies/Booking/BookingViewController.swift
+++ The_Oscars/Features/Movies/Booking/BookingViewController.swift
@@ -16,7 +16,6 @@ class MovieBookingViewController: UIViewController {
         label.text = "예매하기"
         label.font = UIFont.boldSystemFont(ofSize: 24)
         label.textColor = .systemBlue
-        label.textAlignment = .left // 좌측 정렬
         return label
     }()
     
@@ -41,6 +40,30 @@ class MovieBookingViewController: UIViewController {
         return label
     }()
     
+    let countLabel: UILabel = { // 인원 수 표시
+        let label = UILabel()
+        label.text = "1"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let minusButton: UIButton = { // 마이너스 버튼
+        let button = UIButton(type: .system)
+        button.setTitle("-", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        button.setTitleColor(.systemRed, for: .normal)
+        return button
+    }()
+    
+    let plusButton: UIButton = { // 플러스 버튼
+        let button = UIButton(type: .system)
+        button.setTitle("+", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        button.setTitleColor(.systemGreen, for: .normal)
+        return button
+    }()
+    
     let priceLabel: UILabel = {
         let label = UILabel()
         label.text = "총 가격"
@@ -58,10 +81,18 @@ class MovieBookingViewController: UIViewController {
         return button
     }()
     
+    // MARK: - Variables
+    var peopleCount: Int = 1 {
+        didSet {
+            countLabel.text = "\(peopleCount)"
+        }
+    }
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        setupActions()
     }
     
     // MARK: - Setup Views
@@ -69,41 +100,78 @@ class MovieBookingViewController: UIViewController {
         view.backgroundColor = .white
         
         // Add Subviews
-        [titleLabel, movieNameLabel, dateLabel, peopleLabel, priceLabel, bookButton].forEach {
+        [titleLabel, movieNameLabel, dateLabel, peopleLabel, countLabel, minusButton, plusButton, priceLabel, bookButton].forEach {
             view.addSubview($0)
         }
         
-        // Layout with SnapKit
+        // Layout
         titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(60)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(80)
             make.leading.equalToSuperview().offset(20)
         }
         
         movieNameLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(60)
+            make.top.equalTo(titleLabel.snp.bottom).offset(40)
             make.leading.equalToSuperview().offset(20)
         }
         
         dateLabel.snp.makeConstraints { make in
-            make.top.equalTo(movieNameLabel.snp.bottom).offset(60)
+            make.top.equalTo(movieNameLabel.snp.bottom).offset(40)
             make.leading.equalToSuperview().offset(20)
         }
         
         peopleLabel.snp.makeConstraints { make in
-            make.top.equalTo(dateLabel.snp.bottom).offset(60)
+            make.top.equalTo(dateLabel.snp.bottom).offset(40)
             make.leading.equalToSuperview().offset(20)
         }
         
+        // 인원 수와 버튼
+        countLabel.snp.remakeConstraints { make in
+            make.centerY.equalTo(peopleLabel) // 세로 정렬
+            make.centerX.equalToSuperview()   // 화면 중앙에 배치
+            make.width.equalTo(50)
+        }
+
+        minusButton.snp.remakeConstraints { make in
+            make.centerY.equalTo(peopleLabel) // 세로 정렬
+            make.trailing.equalTo(countLabel.snp.leading).offset(-20) // 숫자 왼쪽에 배치
+            make.width.height.equalTo(40)
+        }
+
+        plusButton.snp.remakeConstraints { make in
+            make.centerY.equalTo(peopleLabel) // 세로 정렬
+            make.leading.equalTo(countLabel.snp.trailing).offset(20) // 숫자 오른쪽에 배치
+            make.width.height.equalTo(40)
+        }
+        
         priceLabel.snp.makeConstraints { make in
-            make.top.equalTo(peopleLabel.snp.bottom).offset(60)
+            make.top.equalTo(peopleLabel.snp.bottom).offset(40)
             make.leading.equalToSuperview().offset(20)
         }
         
         bookButton.snp.makeConstraints { make in
-            make.top.equalTo(priceLabel.snp.bottom).offset(60)
+            make.top.equalTo(priceLabel.snp.bottom).offset(40)
             make.leading.equalToSuperview().offset(20)
             make.trailing.equalToSuperview().offset(-20)
             make.height.equalTo(50)
+        }
+    }
+    
+    // MARK: - Actions
+    private func setupActions() {
+        plusButton.addTarget(self, action: #selector(increasePeopleCount), for: .touchUpInside)
+        minusButton.addTarget(self, action: #selector(decreasePeopleCount), for: .touchUpInside)
+    }
+    
+    @objc private func increasePeopleCount() {
+        if peopleCount < 10 { // 최대 인원 수 제한
+            peopleCount += 1
+        }
+    }
+    
+    @objc private func decreasePeopleCount() {
+        if peopleCount > 1 { // 최소 인원 수 제한
+            peopleCount -= 1
         }
     }
 }
