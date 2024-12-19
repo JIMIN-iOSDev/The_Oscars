787d85cf0b41796512a02e2fa0d3e5accaddb09f
“hyunwook-sevo” <seojy1276@naver.com>
2024-12-19 09:39:26 +0900

    feat: 회원가입 화면과 로그인 화면 연결

--- The_Oscars/Features/Auth/Login/LoginViewController.swift
+++ The_Oscars/Features/Auth/Login/LoginViewController.swift
@@ -27,16 +27,45 @@ class LoginViewController: UIViewController {
         loginView.signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
         loginView.findIdPasswordButton.addTarget(self, action: #selector(findIdPasswordButtonTapped), for: .touchUpInside)
     }
+    
+    private func navigateToMyPage() {
+        let myPageVC = MyPageViewController() // MyPageViewController는 미리 생성된 상태여야 합니다.
+        myPageVC.modalPresentationStyle = .fullScreen
+        present(myPageVC, animated: true, completion: nil)
+    }
+    
+    private func showAlert(message: String, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { _ in
+            completion?()
+        }))
+        present(alert, animated: true, completion: nil)
+    }
 
     @objc private func loginButtonTapped() {
-        print("로그인 버튼이 눌렸습니다.")
+        guard let id = loginView.idTextField.text, !id.isEmpty,
+              let password = loginView.passwordTextField.text, !password.isEmpty else {
+            showAlert(message: "아이디와 비밀번호를 입력해주세요.")
+            return
+        }
+        
+        let success = UserDefaultsManager.shared.loginUser(id: id, password: password)
+        
+        if success {
+            showAlert(message: "로그인 성공!", completion: {
+                self.navigateToMyPage()
+            })
+        } else {
+            showAlert(message: "아이디 또는 비밀번호가 잘못되었습니다.")
+        }
     }
     
     @objc private func signUpButtonTapped() {
-        print("회원가입 버튼이 눌렸습니다.")
+        let signUpVC = SignupViewController()
+        present(signUpVC, animated: true, completion: nil)
     }
     
-    @objc private func findIdPasswordButtonTapped() {
-        print("아이디/비밀번호 찾기 버튼이 눌렸습니다.")
-    }
+//    @objc private func findIdPasswordButtonTapped() {
+//        print("아이디/비밀번호 찾기 버튼이 눌렸습니다.")
+//    }
 }
